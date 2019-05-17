5bda4be7514461f0325902e87cdc3a648f18c1fd
Nikunj Polara <npolara@affirma.com>
2019-05-17 17:40:08 +0530

    commit changes

---
+++ Shaheen/Shaheen/BLL/AgentModel.cs
@@ -0,0 +1,14 @@
+﻿using Shaheen.ShaheenDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaheen
+{
+    public class AgentModel : Agent
+    {
+        public string agentName { get; set; }
+    }
+}
