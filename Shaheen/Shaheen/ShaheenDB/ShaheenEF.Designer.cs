f602575c37266a641c19fdf27d66fa050f5001ec
ashrafali002 <ashrafali_002@hotmail.com>
2019-07-26 09:08:22 +0530

    shaheen changes

--- Shaheen/Shaheen/ShaheenDB/ShaheenEF.Designer.cs
+++ Shaheen/Shaheen/ShaheenDB/ShaheenEF.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Ashraf\Shaheen\Shaheen\Shaheen\Shaheen\ShaheenDB\ShaheenEF.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Ashraf\Shaheen\Shaheen\Shaheen\ShaheenDB\ShaheenEF.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
